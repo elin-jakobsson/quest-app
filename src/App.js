10bf544836e9c732a2fc918b5e28bb7eaf3e2528
Elin Jakobsson <elin-jakobsson@hotmail.com>
2018-05-07 16:31:32 +0200

    quest branch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,42 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Profile from './components/profile/profile'
+import Questions from './components/questions/questions'
+
+// Test array från firebase
+const questionArray = [{quest:'Vad står HTML för?',
+                        alternativ:[
+                        'Hyper Tacktic Marmelade Logic',
+                        'Herrod The Magic Lunytoon',
+                        'Hyper Text Markup Language'
+                        ]
+                      },
+                      {quest:'Är HTML ett programeringsspråk?',
+                      alternativ:['Ja', 'Nej'
+                      ]
+                      }];
 
 class App extends Component {
+  constructor(props){
+    super(props)
+  }
+  componentDidMount(){
+    // call for firebase or ajax here
+    //start all calls
+    // kalla på db.ref () med en färdig funktion, referera till en existerande funktion.
+  }
+  componentWillUnmount(){
+    // close all calls
+    // hur vat man att den stängs?
+  }
+  componentDidCatch(error, info){
+    // fånga error
+  }
   render() {
     return (
       <div className="App">
         <Profile/>
+        <Questions questionArray={questionArray}/>
       </div>
     );
   }
---
+++ src/components/questions/questions.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './questions.css';
+
+
+class Questions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionArray: props.questionArray
+
+    }
+  }
+
+  // Shuffle function send in object and the function shuffles the in a random order
+  shuffleArray=(array)=>{
+    // currentIndex the length of the array
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    console.log(currentIndex);
+ // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+   randomIndex = Math.floor(Math.random() * currentIndex);
+   currentIndex -= 1;
+
+   temporaryValue = array[currentIndex];
+   array[currentIndex] = array[randomIndex];
+   array[randomIndex] = temporaryValue;
+
+ }
+
+ // Fåge display för testvarianten
+  let questElements = array.map((x,index) => {
+      let alternativList = x.alternativ.map((z)=>{
+        return(
+        <label key={'key'+z.index}><input type="radio" name={'quiz'+z.index} value=""/>{z}</label>
+      )});
+    return(<div key={index}>
+       <h1>{x.quest}</h1>
+       <form>
+       {alternativList}
+       </form>
+   </div>);
+ });
+
+ return questElements;
+}
+
+  render() {
+    return (
+      <div>
+
+        {this.shuffleArray(this.state.questionArray)}
+
+      </div>
+    );
+  }
+}
+
+export default Questions;
