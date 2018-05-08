46115e31103283f9048eaf96915085574c2318b5
Johan Augustsson <j.augustsson80@gmail.com>
2018-05-08 10:16:53 +0200

    firebase init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import * as firebase from 'firebase';
 import Profile from './components/profile/profile'
+
+var config = {
+   apiKey: "AIzaSyAP1ELuVASv1aMYuXvbQk8N5B-fjIzNWP4",
+   authDomain: "quest-app-lab32.firebaseapp.com",
+   databaseURL: "https://quest-app-lab32.firebaseio.com",
+   projectId: "quest-app-lab32",
+   storageBucket: "quest-app-lab32.appspot.com",
+   messagingSenderId: "85647486236"
+ };
+firebase.initializeApp(config);
+
+firebase.database().ref().once('value').then(snap=>{
+  let data = snap.val();
+  console.log(data);
+})
+/*
+firebase.database().ref('users/').once('value').then(snap=>{
+  let data = snap.val();
+  console.log(data);
+})
+firebase.database().ref('quests/').once('value').then(snap=>{
+  let data = snap.val();
+  console.log(data);
+})
+*/
+//var newPostKey = firebase.database().ref('Games/').child('posts').push().key;   // hämtar ny nyckel
+
 
 class App extends Component {
   render() {
