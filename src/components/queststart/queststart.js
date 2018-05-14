6144ac60beff770f873f6034ce1912e8d805feb6
johan augustsson <j.augustsson80@gmail.com>
2018-05-14 18:34:24 +0200

    categories colours change

--- src/components/queststart/queststart.js
+++ src/components/queststart/queststart.js
@@ -4,18 +4,31 @@ import './queststart.css';
 
 const QuestStart = props =>{
 
+  let obj = {}
+  if(props.selectedCategori==="html"){
+    obj.category = "html";
+    obj.colour = "#60AB25";
+  }else if (props.selectedCategori==="css") {
+    obj.category = "css";
+    obj.colour = "#477AA4";
+  } else {
+    obj.category = "js";
+    obj.colour = "#F7BF35";
+  }
+
+  console.log(obj);
   this.handleStartGame=()=>{
-    console.log("skicka upp ett värde till parent!!");
     props.updatePlayerReady(true);
   }
+
   return(
     <div className="component container-queststart">
       <div className="wrapper">
         <div className='goBackToCategoris'><p onClick={()=>props.chooseCategori("")}>&times;</p></div>
-        <div className="box">Js</div>
+        <div className="box" style= {{backgroundColor : obj.colour}}>{ obj.category }</div>
         <div>Javascript Quests</div>
         <span>Redo!</span>
-        <button onClick={this.handleStartGame}>Start Spelet</button>
+        <button onClick={this.handleStartGame}>Start</button>
       </div>
     </div>
   )
